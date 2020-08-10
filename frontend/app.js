efffc3375f6f77be87635134d1077be07fd38f0b
Ben <shilinchen1010@gmail.com>
2020-08-09 17:38:28 -0700

    env port

--- frontend/app.js
+++ frontend/app.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 // Make everything under public folder available to the outside world
 // Don't put anything sensitive here.
 // Examples: html files, css files, javascript (frontend), images
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/news.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/news.html');
 });
 
-app.route("/register").get(function(req,res) {
-  res.sendFile(__dirname + "/public/register.html");
+app.route('/register').get(function (req, res) {
+  res.sendFile(__dirname + '/public/register.html');
 });
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   console.log(`Listening on port: ${port}`);
 });
