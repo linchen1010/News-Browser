dfcd63dac8137ec6b19122dc86dec4cc40009170
Ben <shilinchen1010@gmail.com>
2021-03-05 10:47:50 -0800

    edit invalid test

--- test/api_test.js
+++ test/api_test.js
@@ -1,12 +1,12 @@
 const fetch = require("node-fetch");
 const apiKey = '504949294bc7419d83beebf27c708229';
 let assert = require('assert');
-const { response } = require("express");
 
 let categories_valid = ['general', 'business', 'entertainment', 'health', 'science', 'technology'];
 let categories_invalid = ['sports', 'class', 'tech', 'trend']
 let search_valid = ['facebook', 'covid-19', 'usc', 'california', 'iphoneSE']
 let search_invalid = ['qwertyo', '---', 'a-d-111,,dc']
+
 function fetch_news_data_category(category) {
     it(`should get response data from newsApi in category: "${category}"`, async() => {
         const url = 'http://newsapi.org/v2/top-headlines?' +
@@ -16,7 +16,7 @@ function fetch_news_data_category(category) {
         await fetch(url)
             .then(res => res.json())
             .then((res) => {
-                console.log(res.totalResults);
+                console.log(res);
                 assert.notEqual(res.totalResults, 0);
             })
     })
@@ -74,7 +74,7 @@ describe('newsApi_category_test', () => {
 })
 
 describe('newsApi_category_invalid_test', () => {
-    for(category of categories_valid) {
+    for(category of categories_invalid) {
         fetch_news_data_category(category);
     }
 })
